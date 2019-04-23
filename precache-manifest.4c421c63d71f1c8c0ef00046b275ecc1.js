66f0303e768a9ce8d0cd4349f6f47a79ee3ca2b8
NinaRoun <nina_roun@mail.ru>
2019-04-23 18:11:43 +0700

    Updates

---
+++ precache-manifest.4c421c63d71f1c8c0ef00046b275ecc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47010c8ca220f2679dbc",
+    "url": "/login-profile-news/static/js/runtime~main.0a6db134.js"
+  },
+  {
+    "revision": "891a225372bf78c9de2e",
+    "url": "/login-profile-news/static/js/main.1f19face.chunk.js"
+  },
+  {
+    "revision": "123f79098eadd68e0a14",
+    "url": "/login-profile-news/static/js/2.e1cb927f.chunk.js"
+  },
+  {
+    "revision": "891a225372bf78c9de2e",
+    "url": "/login-profile-news/static/css/main.611f9c25.chunk.css"
+  },
+  {
+    "revision": "fb3f309cb7a2210232a1b0e6f34ffb99",
+    "url": "/login-profile-news/index.html"
+  }
+];
